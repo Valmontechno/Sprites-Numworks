fde161a7d705c69bcbbdbb811b51aa7e60557cee
Valmontechno <108832011+valmontechno@users.noreply.github.com>
2024-01-07 18:16:32 +0100

    Add files via upload

--- sprites.py
+++ sprites.py
@@ -2,31 +2,31 @@
 # https://github.com/valmontechno/Sprites-Numworks
 
 __name__ = 'sprites'
-__version__ = '1.0'
+__version__ = '1.1'
 
 from kandinsky import *
 
 SCREEN_WIDTH = 320
 SCREEN_HEIGHT = 222
 
-COLOR_CODE = 'abcdefghijklmnopqrstuvwxyz'
-BLANK_CODE = '.'
-NEWLINE_CODE = ','
+COLOR_CHAR = 'abcdefghijklmnopqrstuvwxyz'
+BLANK_CHAR = '.'
+NEWLINE_CHAR = ','
 
 def drawSprite(sprite, pallet, x, y, scale=(1, 1)):
     if isinstance(scale, (int, float)):
         scale = [scale] * 2
     yPos = y
-    for col in sprite.split(NEWLINE_CODE):
+    for col in sprite.split(NEWLINE_CHAR):
         xPos = x
         factorStr = '0'
-        for letter in col:
-            if letter in '0123456789':
-                factorStr += letter
+        for char in col:
+            if char in '0123456789':
+                factorStr += char
             else:
                 factor = max(int(factorStr), 1)
-                if letter != BLANK_CODE:
-                    color = pallet[COLOR_CODE.index(letter)]
+                if char != BLANK_CHAR:
+                    color = pallet[COLOR_CHAR.index(char)]
                     fill_rect(int(xPos), int(yPos), int(scale[0] * factor), int(scale[1]), color)
                 xPos += scale[0] * factor
                 factorStr = '0'
